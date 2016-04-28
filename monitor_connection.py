6875a64eb07bfa72490ca225472e0b3b09283ad1
Pete Graham <git@petegraham.co.uk>
2016-04-28 14:52:12 +0100

    Restart network command in

--- monitor_connection.py
+++ monitor_connection.py
@@ -1,50 +1,60 @@
-"""
-http://stackoverflow.com/questions/2953462/pinging-servers-in-python
-"""
 import os
 from datetime import datetime
+from subprocess import call
 from time import sleep
 
-hostname = 'google.com'
-# Wait time 2 seconds to allow for slow response or delays
-ping_command = 'ping -c 1 -W 2 {} > /dev/null'.format(hostname)
-last_response = None  # Initialise
-down_count = 0
+DOWN_COUNT_LIMIT = 5
+SLEEP_TIME_SECONDS = 2.5
 
-while True:
-    response = os.system(ping_command)
-    connection = response == 0
 
-    if last_response != response:
+def connection_check(hostname='google.com'):
+    """
+    Do we currently have a internet connection?
+    Pings host with one packet.
+    Allows a 2 second wait time for slow networks.
+    http://stackoverflow.com/questions/2953462/pinging-servers-in-python
+    """
+    devnull = open(os.devnull, 'w')
+    response = call(
+        ['ping', '-c', '1', '-W', '2', hostname],
+        stdout=devnull,
+        stderr=devnull)
+    return response == 0
+
+
+def connection_changed(connnection, last_connection, down_count):
+    """
+    Print if the connection status has changed
+    """
+    if last_connection != connection:
         if connection:
             print('{} Internet is connected. Down count {}'.format(
                 datetime.now(), down_count))
         else:
-            # Don't need down count here
             print('{} Internet down. Down count {}'.format(
                 datetime.now(), down_count))
-            # TODO: fix with ifconfig en0 down; ifconfig en0 up
-            # sudo ifconfig en0 down; sudo ifconfig en0 up
-            # TODO: track intetenet outage here
+
+
+def reconnect_to_network():
+    """
+    You will need to run sudo ifconfig without needing to input a password for
+    this to work
+    """
+    os.system('sudo ifconfig en0 down; sudo ifconfig en0 up')
+
+
+last_connection = None
+down_count = 0
+while True:
+    connection = connection_check()
+    connection_changed(connection, last_connection, down_count)
+    last_connection = connection
 
     if connection is False:
         down_count += 1
     else:
         down_count = 0
 
-    # TODO: restart here based on down count > 5
-
-    last_response = response
-    sleep(2.5)
-
-# do
-#   yup=`ping -c1 -t2 www.google.com|grep from`
-#   if [[ -n $yup ]]
-#      then echo "Internet is connected!"
-#   else
-#         ifconfig en0 down; ifconfig en0 up
-#         echo "Down at `date +%m%d%Y%H%M%S`">>./dropped.out
-#   fi
-#   sleep 5
-# clear
-# done
+    if down_count > DOWN_COUNT_LIMIT:
+        reconnect_to_network()
+    sleep(SLEEP_TIME_SECONDS)
